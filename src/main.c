0c37eb6d29d6ab58461aceef55efb9d0319d34df
Abhinav Borah <imabhinavborah@gmail.com>
2024-05-18 17:41:38 +0530

    add color buffer and game loop with garbage collection

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
+#include "SDL2/SDL_render.h"
+#include "SDL2/SDL_shape.h"
+#include "SDL2/SDL_video.h"
 #include <SDL2/SDL.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 
 bool is_running = false;
+
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
+
+uint32_t *color_buffer = NULL;
+SDL_Texture *color_buffer_texture = NULL;
+
+int window_width = 800;
+int window_height = 600;
 
 bool initialize_window(void) {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -12,10 +23,16 @@ bool initialize_window(void) {
     return false;
   };
 
+  // Get the current display mode
+  SDL_DisplayMode display_mode;
+  SDL_GetCurrentDisplayMode(0, &display_mode);
+  window_width = display_mode.w;
+  window_height = display_mode.h;
+
   // Creates an SDL window
   window =
       SDL_CreateWindow(NULL, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       800, 600, SDL_WINDOW_RESIZABLE);
+                       window_width, window_height, SDL_WINDOW_RESIZABLE);
 
   if (!window) {
     fprintf(stderr, "Error creating SDL window.\n");
@@ -30,13 +47,22 @@ bool initialize_window(void) {
     return false;
   }
 
+  // Set the window to fullscreen
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
   return true;
 }
 
+// Color Buffer to store the pixel values
 void setup(void) {
-  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-  SDL_RenderClear(renderer);
-  SDL_RenderPresent(renderer);
+  // Allocate memory for the color buffer
+  color_buffer =
+      (uint32_t *)malloc(sizeof(uint32_t) * window_width * window_height);
+
+  // Create an SDL texture to display the color buffer
+  color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                                           SDL_TEXTUREACCESS_STREAMING,
+                                           window_width, window_height);
 }
 
 void process_input(void) {
@@ -58,11 +84,36 @@ void process_input(void) {
 
 void update(void) {}
 
+void render_color_buffer(void) {
+  SDL_UpdateTexture(color_buffer_texture, NULL, color_buffer,
+                    window_width * sizeof(uint32_t));
+  SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color) {
+  for (int y = 0; y < window_height; y++) {
+    for (int x = 0; x < window_width; x++) {
+      color_buffer[(y * window_width) + x] = color;
+    }
+  }
+}
+
 void render(void) {
-  SDL_SetRenderDrawColor(renderer, 255, 255, 0, 1);
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 1);
   SDL_RenderClear(renderer);
 
+  render_color_buffer();
+  clear_color_buffer(0xff000000);
+
   SDL_RenderPresent(renderer);
+}
+
+// Memory Deallocation
+void destroy_window(void) {
+  free(color_buffer);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
 }
 
 int main(void) {
@@ -76,5 +127,6 @@ int main(void) {
     render();
   }
 
+  destroy_window();
   return 0;
 }
